db1169c3dd863c3d55a0d3bf47f9bf53ffca1f95
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2024-04-16 12:44:16 -0600

    Add auth astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), auth()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
 });
