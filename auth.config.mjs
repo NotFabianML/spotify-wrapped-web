616a4cc0ae428aea6d197d2b3b19ca84684fd5dc
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2024-04-22 23:02:18 -0600

    Add parameters

--- auth.config.mjs
+++ auth.config.mjs
@@ -1,22 +1,24 @@
-import Spotify from '@auth/core/providers/spotify'
-import { defineConfig } from 'auth-astro'
-
-
+import Spotify from "@auth/core/providers/spotify";
+import { defineConfig } from "auth-astro";
 
 export default defineConfig({
-	providers: [
-		Spotify({
-			clientId: import.meta.env.SPOTIFY_CLIENT_ID,
-			clientSecret: import.meta.env.SPOTIFY_CLIENT_SECRET,
-		}),
-	],
-	callbacks: {
-		session: ({ session, token }) => ({
-			...session,
-			user: {
-				...session.user,
-				id: token.sub,
-			},
-		}),
-	},
+  providers: [
+    Spotify({
+      clientId: import.meta.env.SPOTIFY_CLIENT_ID,
+      clientSecret: import.meta.env.SPOTIFY_CLIENT_SECRET,
+      scope: "user-top-read",
+      authorizeUrl: "https://accounts.spotify.com/authorize",
+      tokenUrl: "https://accounts.spotify.com/api/token",
+      redirectUri: "http://localhost:3000/auth/callback",
+    }),
+  ],
+  callbacks: {
+    session: ({ session, token }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: token.sub,
+      },
+    }),
+  },
 });
